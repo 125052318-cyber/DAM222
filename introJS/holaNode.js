5568400b5f3e895865fac01ccdc72dbed58e6758
Uriel Manzano <125052318@upq.edu.mx>
2026-09-14 18:58:36 -0600

    se crearon 3 archivos y en cada uno se programo la practica guiada

---
+++ introJS/holaNode.js
@@ -0,0 +1,14 @@
+
+console.log("Hola Node.js");
+
+let edad1 = 19;
+let edad2 = 20;
+
+console.log("Edad promedio: ");
+console.log((edad1 + edad2) / 2);
+
+/*Medir tiempo de un proceso */
+console.time("miProceso");
+    for(let i = 0; i < 10000000; i++){
+    };
+console.timeEnd("miProceso");
