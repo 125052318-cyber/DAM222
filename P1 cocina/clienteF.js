c5009c3ebf7949b56036c056278366009cf8c1d0
Uriel Manzano <125052318@upq.edu.mx>
2026-09-19 20:09:13 -0600

    Acabe con mi parte del modulo, terminando asi mi codigo

--- P1 cocina/clienteF.js
+++ P1 cocina/clienteF.js
@@ -27,3 +27,98 @@ let productos =[
     }
 ]
 
+let pedidos = [];
+
+menu();
+
+// funciones a utilizar en el programa
+function mostrarMenu(){
+    console.log("MENÚ PRINCIPAL");
+    console.log("1. Cosultar los productos");
+    console.log("2. Crear un pedido");
+    console.log("3. Listar los pedidos");
+    console.log("4. Salir del programa");
+    console.log("");
+}
+
+function consultarProductos() {
+    console.log("");
+    console.log("=== PRODUCTOS ===");
+    productos.forEach(function(producto) {
+        console.log(
+            `${producto.id}. ${producto.nombre} - $${producto.precio}`
+        );
+
+    });
+}
+
+async function crearPedido() {
+
+    consultarProductos();
+
+    let id = await rl.question("\nEscribe el ID del producto: ");
+    let cantidad = await rl.question("Escribe la cantidad: ");
+
+    let producto = productos.find(function(producto) {
+        return producto.id == id;
+    });
+
+    if (producto) {
+
+        let pedido = {
+            producto: producto.nombre,
+            cantidad: Number(cantidad),
+            total: producto.precio * Number(cantidad)
+        };
+
+        pedidos.push(pedido);
+        console.log("Pedido Creado");
+        console.log("Producto:", producto.nombre);
+        console.log("Cantidad:", cantidad);
+        console.log("Total: $", pedido.total);
+    } else {
+        console.log("Producto no encontrado.");
+    }
+}
+
+function listarPedidos() {
+    console.log("=== PEDIDOS ===");
+    pedidos.forEach(function(pedido) {
+        console.log(
+            `${pedido.producto} x${pedido.cantidad} = $${pedido.total}`
+        );
+    });
+}
+
+async function menu() {
+    let opcion;
+
+    do {
+
+        mostrarMenu();
+        opcion = await rl.question("Elige una opción: ");
+
+        if (opcion == "1") {
+            consultarProductos();
+        }
+
+        else if (opcion == "2") {
+            await crearPedido();
+        }
+
+        else if (opcion == "3") {
+            listarPedidos();
+        }
+
+        else if (opcion == "4") {
+            console.log("Gracias por utilizar el sistema.");
+        }
+
+        else {
+            console.log("Opción no válida.");
+        }
+
+    } while (opcion != "4");
+
+    rl.close();
+}
